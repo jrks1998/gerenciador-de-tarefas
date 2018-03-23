90be303669289775942c6c4dde93b61e6764c69d
klicheti <klicheti@gmail.com>
2018-03-22 20:43:19 -0400

    opcao de fechar e criar novas tarefas

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -3,8 +3,14 @@ class TaskController < ApplicationController
   end
 
   def create
+    Task.create(description:params[:description], finished: false)
+    redirect_to acrtion :'index'
   end
 
   def finished
+    # render text:'blablabla' + params[:id]
+    
+    Task.find_by_id(params[:id]).update_attribute(:finished, true)
+    redirect_to action: 'index'
   end
 end
